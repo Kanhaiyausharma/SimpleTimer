65c3781dfc964d3294e49513f696d135f7759d8b
Kanhaiya Sharma <kanhaiya.sharma@wwindia.com>
2018-03-14 15:04:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webwerks/simpletimer/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.webwerks.simpletimer;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+   private Button startbutton;
+   private Button pausebutton;
+   private Button resetbutton;
+   private Chronometer chronomet;
+   private Long pause;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startbutton = (Button) findViewById(R.id.start);
+        pausebutton = (Button) findViewById(R.id.pause);
+        resetbutton = (Button) findViewById(R.id.stop);
+        chronomet =(Chronometer) findViewById(R.id.timer);
+        pausebutton.setEnabled(false);
+        resetbutton.setEnabled(false);
+
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+             if(pause != 0){
+                 chronomet.setBase(SystemClock.elapsedRealtime());
+             }else{
+                 chronomet.setBase(SystemClock.elapsedRealtime());
+             }
+             chronomet.start();
+             startbutton.setEnabled(false);
+             pausebutton.setEnabled(true);
+            }
+        });
+
+        pausebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pause = SystemClock.elapsedRealtime();
+                chronomet.stop();
+                pausebutton.setEnabled(false);
+                startbutton.setEnabled(true);
+            }
+        });
+
+        resetbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronomet.stop();
+                pausebutton.setEnabled(false);
+                startbutton.setEnabled(true);
+                chronomet.setBase(SystemClock.elapsedRealtime());
+                pause= (long)0;
+
+            }
+        });
+    }
+}
